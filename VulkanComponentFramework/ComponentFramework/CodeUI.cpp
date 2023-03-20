e1f9ba8bf7bad01e3c12beb9276ca921b18a2f70
jadeliao <n01446860@humbermail.ca>
2023-03-19 21:34:22 -0400

    CodeUI Update

--- VulkanComponentFramework/ComponentFramework/CodeUI.cpp
+++ VulkanComponentFramework/ComponentFramework/CodeUI.cpp
@@ -8,6 +8,8 @@ CodeUI::~CodeUI(){
 
 void CodeUI::Display(){
 
+    //if (!showWindow) return;
+
     ImGui::SetNextWindowBgAlpha(windowAlpha);
     ImGui::Begin("CodeUI", &showWindow,
         ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse |
@@ -17,19 +19,19 @@ void CodeUI::Display(){
     //Calculate ui window size
     //It needs to be dependent to the scene window size, so that when resizing the scene window,
     //ui window will change accordingly
-    float windowW = width * 0.4f;
-    float windowH = height * 0.6f;
+    float windowW = width * 0.3f;
+    float windowH = height * 0.55f;
     ImGui::SetWindowSize(ImVec2(windowW, windowH));
     ImGui::SetWindowPos(ImVec2(width / 2.0f - windowW * 0.5f, height / 2.0f - windowH * 0.5f));
 
 
     //Calculate button size and location
     float buttonSizeX = 0.2f;
-    float buttonLocX = windowW * ((1.0f - buttonSizeX) / 2.0f);
-    ImVec2 buttonSize(windowW * buttonSizeX, windowH * 0.1f);
+    float buttonLocX = windowW * ((1.0f - buttonSizeX) / 6.0f);
+    ImVec2 buttonSize(windowW * buttonSizeX, windowH * 0.15f);
     ImVec2 buttonLocation(buttonLocX, windowH * 0.35f);
-    float heightInterval = windowH * 0.13f;
-    float widthInterval = windowW * 0.13f;
+    float heightInterval = windowH * 0.25f;
+    float widthInterval = windowW * 0.2f;
 
     //Calculate Text size and location
     ImGui::SetWindowFontScale(5.0f);
